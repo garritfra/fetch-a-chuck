48cf631fd9b7808f29eb453c63a52fbb5daeebb3
garritfra <garritfranke@gmail.com>
2018-05-19 14:20:20 +0200

    Add fetch

---
+++ app.js
@@ -0,0 +1,11 @@
+const fetch = require("node-fetch");
+
+const randomJoke = () => {
+  return fetch("http://api.icndb.com/jokes/random/")
+    .then(response => response.json())
+    .then(json => {
+      return json.value.joke;
+    });
+};
+
+module.exports = randomJoke();
