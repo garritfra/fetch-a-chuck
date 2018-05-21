4a5bcd371c7310f769d312fc129a9fbbd7a8d0c3
garritfra <garritfranke@gmail.com>
2018-05-21 23:17:05 +0200

    add docu

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const fetch = require("node-fetch");
 
+/**
+ * @returns {Promise} random Chuck Norris Joke
+ */
 const randomJoke = () => {
   return fetch("http://api.icndb.com/jokes/random/")
     .then(response => response.json())
